12290b428f9b4eba34313d14d8776e0f3f1ce7e4
Arthur01 <arthur.jerlstrom@gmail.com>
2025-07-19 22:55:52 +0200

    basic CL argument checks

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,27 @@
-int main(){
+#include <iostream>
+
+int main(int argc, char* argv[]){
+
+    if(argc != 2){
+        std::cout << "invalid number of arguments \n";
+    }
+
+    std::string command = argv[1];
+
+    if(argc == 2){
+        if(command == "init"){
+            std::cout << "initializing repository... \n";
+        }
+        else if(command == "add"){
+            std::cout << "adding file... \n";
+        }
+        else if(command == "commit"){
+            std::cout << "commiting files... \n";
+        }
+        else if(command == "log"){
+            std::cout << "printing files... \n";
+        }
+    }
 
     return 0;
 }
