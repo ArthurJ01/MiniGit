9aa0d77bf0c0652a1d75788118b492e3cac32077
Arthur01 <arthur.jerlstrom@gmail.com>
2025-07-23 16:06:41 +0200

    Reworked add to add right content to file
    Still needs to work on TREE object creation and finding the objects folder path

--- src/main.cpp
+++ src/main.cpp
@@ -9,41 +9,34 @@
 
 enum class FileType{BLOB, TREE};
 
-struct FileData {
-    FileType fileType;
-    std::string content;
-
-    FileData(const FileType& fileType, const std::string& content)
-        : fileType(fileType), content(content) {}
-};
-
-std::string hashObject(const FileData& fileData){
-
-    std::stringstream os;
-    std::string fileType;
-    if (fileData.fileType == FileType::BLOB){
-        fileType = "blob ";
-    }
-    else if (fileData.fileType == FileType::TREE){
-        fileType = "tree ";
-    }
-
-    os << fileType << fileData.content.size() << '\0';
-    os << fileData.content;
-
+std::string hashObject(const std::string& fileData){
     SHA1 sha1;
-    sha1.update(os.str());
+    sha1.update(fileData);
     return sha1.final();
 }
 
-std::string serializeFile(const std::filesystem::path& filePath){
+const std::string serializeFile(const std::filesystem::path& filePath, const FileType& fileType){
     std::ifstream file(filePath, std::ios::in | std::ios::binary);
     if(!file){
         throw std::runtime_error("Failed to open file: " + filePath.string());
     }
     std::ostringstream content;
+    std::ostringstream serializedFile;
+    std::ostringstream fileHeader;
+
     content << file.rdbuf();
-    return content.str();
+
+    switch(fileType){
+        case FileType::BLOB :
+            fileHeader << "blob " << content.str().size() << '\0';
+        break;
+
+        case FileType::TREE :
+            fileHeader << "tree " << content.str().size() << '\0';
+        break;
+    }
+    serializedFile << fileHeader.str() << content.str();
+    return serializedFile.str();
 }
 
 void add(char* argv[]){
@@ -52,10 +45,30 @@ void add(char* argv[]){
         return;
     }
     std::filesystem::path filePath = argv[2];
-    FileType fileType = FileType::BLOB;
-    std::string serializedFile = serializeFile(filePath);
-    FileData fileData (fileType, serializedFile);
-    std::string blob = hashObject(fileData);
+
+    // TODO: this should be the objects folder path
+    std::filesystem::path objectFolderPath = filePath;
+
+    std::string hashedObject;
+
+    if(std::filesystem::is_directory(filePath)){
+        std::string serializedContent = serializeFile(filePath, FileType::TREE);
+    }
+    else{
+        std::string serializedContent = serializeFile(filePath, FileType::BLOB);
+        hashedObject = hashObject(serializedContent);
+        
+        std::filesystem::path objectPath = objectFolderPath / hashedObject;
+
+        if(!std::filesystem::exists(objectPath)){
+            std::ofstream file(objectPath, std::ios::binary);
+            if (!file) {
+                std::cerr << "Failed to create HEAD file\n";
+            } else {
+                file << serializedContent;
+            }
+        }
+    }
 }
 
 int main(int argc, char* argv[]){
