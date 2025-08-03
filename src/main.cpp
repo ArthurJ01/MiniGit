b7b92e2fe13ed43b9d594202d7b05d91f7d7d719
Arthur01 <arthur.jerlstrom@gmail.com>
2025-08-03 18:51:24 +0200

    THE PLAN

--- src/main.cpp
+++ src/main.cpp
@@ -4,42 +4,14 @@
 #include "add.hpp"
 #include "commit.hpp"
 #include "log.hpp"
+#include "branch.hpp"
 
 void checkout(char* argv[]){
-    
-}
-
-void branch(char* argv[]){
-
-    if(argv[2] == nullptr){
-        std::cerr << "Usage: branch <branch-name>\n";
-        return;
-    }
-    std::string branchName = argv[2];
-    std::filesystem::path repositoryRoot = findRepositoryRoot(std::filesystem::current_path());
-    std::filesystem::path currentBranchPath = getCurrentBranchPath(repositoryRoot);
-    std::filesystem::path newBranchPath = repositoryRoot / ".minigit" / "refs" / "heads" / branchName;
-    if(!createOrOverwriteFile(newBranchPath)) return;
-
-    //copy current branch file contents
-    std::ifstream currentBranchFile(currentBranchPath, std::ios::binary);
-    if(!currentBranchFile){
-        std::cerr << "failed to open " << currentBranchPath << "\n";
-        return;
-    }
-    std::stringstream fileContents;
-    fileContents << currentBranchFile.rdbuf();
-    currentBranchFile.close();
-
-    //paste the content into the new branch file
-    std::ofstream newBranchFile(newBranchPath, std::ios::binary);
-    if(!newBranchFile){
-        std::cerr << "failed to open " << newBranchPath << "\n";
-        return;
-    }
-    newBranchFile << fileContents.str();
-
-    std::cout << "new branch: " << newBranchPath.filename() << " created.\n";
+    //warn if uncommited files exist, refuse to checkout
+    //if checkout on commit, store commit in detached HEAD (do this later)
+    //if checkout on branch, change path in HEAD file
+    //delete all content in the repo
+    //add all content from commit in wanted branch
 }
 
 int main(int argc, char* argv[]){
