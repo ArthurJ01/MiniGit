ab29d8eb93ebace1acdc6c5c4965dcddc86c5d00
Arthur01 <arthur.jerlstrom@gmail.com>
2025-08-08 17:10:27 +0200

    now commits old unchanged files

--- src/commit.cpp
+++ src/commit.cpp
@@ -5,6 +5,61 @@
 
 #include "util.hpp"
 #include "commit.hpp"
+#include "checkout.hpp"
+
+std::unordered_map<std::filesystem::path, std::string> loadLastCommit(const std::filesystem::path& repositoryRoot, const std::string& parentCommitHash){
+
+    std::unordered_map<std::filesystem::path, std::string> commitMap;
+    if(parentCommitHash == ""){ 
+        return commitMap;
+    }
+
+    std::filesystem::path objectFolderPath = repositoryRoot / ".minigit" / "objects";
+    std::filesystem::path parentCommitPath = objectFolderPath / parentCommitHash;
+    std::ifstream parentCommit(parentCommitPath, std::ios::binary);
+    std::string line;
+    std::string lastCommitHash = "";
+
+    while (std::getline(parentCommit, line)) {
+        size_t spacePos = line.find(' ');
+        if (spacePos != std::string::npos) {        
+            std::string title = line.substr(0, spacePos);
+            std::string content = line.substr(spacePos + 1);
+            if(title == "tree"){
+                lastCommitHash = content;
+            }
+        }
+    }
+    
+    parentCommit.close();
+    std::filesystem::path treePath = objectFolderPath / lastCommitHash;
+
+    std::stack<treeBlob> listOfBlobs;
+    findAllFiles(listOfBlobs, treePath, repositoryRoot);
+
+    while (!listOfBlobs.empty()){
+        treeBlob currentBlob = listOfBlobs.top();
+        listOfBlobs.pop();
+
+        //normal file
+        if(currentBlob.fileMode == "100644"){
+            std::filesystem::path relativePath = std::filesystem::relative(currentBlob.fullPath, repositoryRoot);
+            commitMap[relativePath] = currentBlob.hash;
+        }
+        //directory
+        else if(currentBlob.fileMode == "040000"){
+            findAllFiles(listOfBlobs, objectFolderPath / currentBlob.hash, currentBlob.fullPath);
+        }
+        else{
+            //filemode not implemented
+            //100755 executable
+            //120000 symbolic link
+        }
+    }
+
+    return commitMap;
+    
+}
 
 //needs optimisation and, needs to add untracked files that were in last commit that haven't changed.
 void commit (char* argv[]){
@@ -16,10 +71,12 @@ void commit (char* argv[]){
     std::filesystem::path indexFilePath = repositoryRoot / ".minigit" / "index";
 
     //load index into hashmap for lookups, <pathString, hash>
-    std::unordered_map<std::string, std::string> indexMap = loadIndex(indexFilePath);
-    blob commit = createCommitTree(repositoryRoot, repositoryRoot, indexMap);
-    
     std::string parentCommitHash = getParentCommitHash(repositoryRoot);
+    std::unordered_map<std::filesystem::path, std::string> indexMap = loadIndex(indexFilePath);
+    std::unordered_map<std::filesystem::path, std::string> lastCommitMap = loadLastCommit(repositoryRoot, parentCommitHash);
+    blob commit = createCommitTree(repositoryRoot, repositoryRoot, indexMap, lastCommitMap);
+    
+
 
     std::stringstream commitContents;
     commitContents << "tree " << commit.hash << "\n";
@@ -42,8 +99,8 @@ void commit (char* argv[]){
     indexFileWrite.close();
 }
 
-std::unordered_map<std::string, std::string> loadIndex(const std::filesystem::path& indexPath) {
-    std::unordered_map<std::string, std::string> indexMap;
+std::unordered_map<std::filesystem::path, std::string> loadIndex(const std::filesystem::path& indexPath) {
+    std::unordered_map<std::filesystem::path, std::string> indexMap;
     std::ifstream indexFile(indexPath);
     if (!indexFile.is_open()) {
         std::cerr << "Could not open index file\n";
@@ -54,13 +111,14 @@ std::unordered_map<std::string, std::string> loadIndex(const std::filesystem::pa
         auto commaPos = line.find(',');
         if (commaPos == std::string::npos) continue;
         std::string filePathStr = line.substr(0, commaPos);
+        std::filesystem::path filePath = filePathStr;
         std::string hash = line.substr(commaPos + 1);
-        indexMap[filePathStr] = hash;
+        indexMap[filePath] = hash;
     }
     return indexMap;
 }
 
-blob createCommitTree(std::filesystem::path filePath, std::filesystem::path repositoryRoot, const std::unordered_map<std::string, std::string>& indexMap){
+blob createCommitTree(std::filesystem::path filePath, std::filesystem::path repositoryRoot, const std::unordered_map<std::filesystem::path, std::string>& indexMap, const std::unordered_map<std::filesystem::path, std::string>& lastCommitMap){
 
     if (!std::filesystem::is_directory(filePath)) {
         throw std::runtime_error("createCommitTree() called on a non-directory path: " + filePath.string());
@@ -86,7 +144,7 @@ blob createCommitTree(std::filesystem::path filePath, std::filesystem::path repo
                 continue;
             }
             else{
-                blob possibleBlob = createCommitTree(entry, repositoryRoot, indexMap);
+                blob possibleBlob = createCommitTree(entry, repositoryRoot, indexMap, lastCommitMap);
                 if(possibleBlob.fileType == FileType::EMPTY){
                     continue;
                 }
@@ -94,15 +152,15 @@ blob createCommitTree(std::filesystem::path filePath, std::filesystem::path repo
             }   
         }
         else if(std::filesystem::is_regular_file(entry)){
-            std::string relativePath = std::filesystem::relative(entry, repositoryRoot).string();
-            //if not in indexmap
-
-            //TODO: add map with all files from last commit to comparison, so we check the indexmap and lastCommitMap
-            if (indexMap.find(relativePath) == indexMap.end()) {
-                continue;
-            }
-            else{
+            std::filesystem::path relativePath = std::filesystem::relative(entry, repositoryRoot);
+            if (indexMap.find(relativePath) != indexMap.end()) {
                 listOfBlobs.emplace_back(indexMap.at(relativePath), entry.filename().string(), FileType::BLOB);
+                std::cout << "commiting file: " << entry.filename() <<"\n";
+            } else if (lastCommitMap.find(relativePath) != lastCommitMap.end()) {
+                listOfBlobs.emplace_back(lastCommitMap.at(relativePath), entry.filename().string(), FileType::BLOB);
+                std::cout << "commiting file: " << entry.filename() <<"\n";
+            } else {
+                continue;
             }
         }
     }
--- src/headers/commit.hpp
+++ src/headers/commit.hpp
@@ -8,6 +8,6 @@
 #include "util.hpp"
 
 void commit (char* argv[]);
-std::unordered_map<std::string, std::string> loadIndex(const std::filesystem::path& indexPath);
-blob createCommitTree(std::filesystem::path filePath, std::filesystem::path repositoryRoot, const std::unordered_map<std::string, std::string>& indexMap);
+std::unordered_map<std::filesystem::path, std::string> loadIndex(const std::filesystem::path& indexPath);
+blob createCommitTree(std::filesystem::path filePath, std::filesystem::path repositoryRoot, const std::unordered_map<std::filesystem::path, std::string>& indexMap, const std::unordered_map<std::filesystem::path, std::string>& lastCommitMap);
 void updateHeadFile(const std::filesystem::path& repositoryRoot, const std::string& hash);
