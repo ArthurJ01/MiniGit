95a4756db2f4dae12e656e636f200cbc23fb007c
Arthur01 <arthur.jerlstrom@gmail.com>
2025-08-19 18:27:36 +0200

    The plan

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,9 @@
 #include "checkout.hpp"
 #include "branch.hpp"
 
-void merge(char* argv[]){
+void rebase(char* argv[]){
     if(argv[2] == nullptr || argv[3] == nullptr){
-        std::cout << "Usage: merge <branch to merge into> <branch to be merged>";
+        std::cout << "Usage: rebase <branch to rebase into> <branch to be merged>";
         return;
     }
 
@@ -17,7 +17,39 @@ void merge(char* argv[]){
     std::filesystem::path branchHeads = repositoryRoot / ".minigit" / "refs" / "heads";
     std::vector<std::filesystem::path> listOfAllBranches;
 
+    try {
+        for (const auto& entry : std::filesystem::directory_iterator(branchHeads)) {
+            listOfAllBranches.emplace_back(entry.path());
+        }
+    } catch (const std::filesystem::filesystem_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    std::string rebaseTarget = argv[2];
+    std::string rebaseSource = argv[3];
+    bool validTargetBranch = false;
+    bool validSourceBranch = false;
+    for(const auto& entry : listOfAllBranches){
+        if(entry.filename() == rebaseTarget){
+            validTargetBranch = true;
+        }
+        else if(entry.filename() == rebaseSource){
+            validSourceBranch = true;
+        }
+    }
+
+    if(!validSourceBranch || !validTargetBranch){
+        std::cout << "Invalid branch name(s)";
+        return;
+    }
+
+    //branchNames are valid now, find fork point (common ancestor) now
+
+    //from fork point, compare source and target branch, create a new tree from this. 
     
+    //When done make new commit, then move on to next commit
+
+
 }
 
 int main(int argc, char* argv[]){
@@ -47,8 +79,8 @@ int main(int argc, char* argv[]){
         else if(command == "branch"){
             branch(argv);
         }
-        else if(command == "merge"){
-            merge(argv);
+        else if(command == "rebase"){
+            rebase(argv);
         }
         else{
             std::cout << "invalid argument: " << command << std::endl;
